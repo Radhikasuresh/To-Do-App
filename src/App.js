8f50ff7920010d40ce5b64902892b6426e088f5e
Radhikasuresh <radhikasuresh550@gmail.com>
2023-08-30 15:19:54 +0530

    changed completed to-do logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import {  Route, Routes, useNavigate } from 'react-router-dom';
-import Todo from './Todo';
-import Completed from './Completed';
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Todo from "./Todo";
+import Completed from "./Completed";
 
 function App() {
- 
   return (
     <Routes>
-    <Route path="/" element={<Todo/>}/>
-    <Route path="/completed" element={<Completed/>}/>
+      <Route path="/" element={<Todo />} />
+      <Route path="/completed" element={<Completed />} />
     </Routes>
   );
 }
--- src/Completed.js
+++ src/Completed.js
@@ -1,25 +1,30 @@
-import { Button, Table } from 'react-bootstrap';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Completed() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const title = queryParams.get('title');
-  const description = queryParams.get('description');
+  const [completedTodos, setCompletedTodos] = useState([]);
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    setCompletedTodos(todos?.filter((todos) => todos.completed));
+  }, []);
 
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  const homeButton=()=>{
-    navigate("/")
-  }
+  const homeButton = () => {
+    navigate("/");
+  };
 
   return (
     <div>
-        <div style={{textAlign:"center"}}>
-      <h1 >Completed To-Do</h1>
-      <Button variant="success" onClick={homeButton}>Go to Home Page</Button>
-      </div><br/>
-      <Table striped bordered hover style={{textAlign:"center"}}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Completed To-Do</h1>
+        <Button variant="success" onClick={homeButton}>
+          Go to Home Page
+        </Button>
+      </div>
+      <br />
+      <Table striped bordered hover style={{ textAlign: "center" }}>
         <thead>
           <tr>
             <th>Title</th>
@@ -27,10 +32,12 @@ function Completed() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{title}</td>
-            <td>{description}</td>
-          </tr>
+          {completedTodos?.map((item) => (
+            <tr>
+              <td>{item?.title}</td>
+              <td>{item?.description}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
